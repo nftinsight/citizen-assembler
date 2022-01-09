491e656a625bf64ea0f56156bb64eb9d2d84896e
nftinsight <97204318+nftinsight@users.noreply.github.com>
2022-01-09 13:52:07 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,6 +10,19 @@ const ipfsMaleBaseUrl = 'https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyV
 const ipfsFemaleBaseUrl = 'https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/';
 const maleBaseUrl = './assets/male';
 const femaleBaseUrl = './assets/female';
+const idRate = {
+  Low: 1,
+  Mid: 3,
+  High: 8,
+}
+const vaultRate = {
+  Low: 1,
+  Medium: 2,
+  "Medium High": 3,
+  High: 4,
+  "Very High": 5,
+  "?": 7,
+}
 
 function App() {
   const [identity, setIdentity] = useState(null);
@@ -17,6 +30,29 @@ function App() {
   const [cache, setCache] = useState(null);
   const [land, setLand] = useState(null);
   const isValidCitizen = identity !== null && cache !== null && land !== null;
+  const isMobile = window.innerWidth <= 1000;
+  const attributes = {
+    Race: identity?.race || null,
+    Gender: identity?.gender || null,
+    Class: identity?.class || null,
+    Location: land?.location || null,
+    Ability: identity?.ability || null,
+    Eyes: identity?.eyes || null,
+    Helm: cache?.helm || null,
+    Apparel: cache?.apparel || null,
+    Weapon: cache?.weapon || null,
+    Vehicle: cache?.vehicle || null,
+    "Additional Item": vault?.additionalItem || null,
+    Yield: identity?.yield || null,
+    "Multiplier": vault?.multiplier || null,
+    "Reward Rate": (vault ? vaultRate[vault.multiplier] : 0) + (identity ? idRate[identity.yield] : 0),
+    Cool: identity?.cool || null,
+    Strength: identity?.strength || null,
+    "Tech Skill": identity?.techSkill || null,
+    Intelligence: identity?.intelligence || null,
+    Attractiveness: identity?.attractiveness || null,
+    "Trait Sum": identity?.sum || null,
+  }
   let componentScore = 0;
   let rank = 'last';
   if (isValidCitizen) {
@@ -34,38 +70,8 @@ function App() {
     }
   }
 
-  // const image = (
-  //     <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 1200 1200">
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/background/10.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/weapon/20-0.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/helm/51-0.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/body/8.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/cloth/51.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/hand/melee/8.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/weapon/20.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/head/8.png"/>
-  //       <image href="https://neotokyo.mypinata.cloud/ipfs/QmPLW6u5MRut1b8iyVc47ET5zAj9VaG2GwyjcuKLoetWsT/helm/51.png"/>
-  //     </svg>
-  // );
-
-
-  let imageM, imageF, bg, weapon1M, weapon2M, weapon1F, weapon2F, helmM1, helmM2, helmF1, helmF2, bodyM, bodyF, clothM1, clothM2, clothF, handM, handF, headM, headF;
+  let imageM, weapon1M, weapon2M, helmM1, helmM2, clothM1, clothM2;
   if (isValidCitizen) {
-    bg = assets.male.backgrounds[land.location];
-    helmM2 = assets.male.helms[cache.helm];
-    helmF1 = assets.male.helms[cache.helm];
-    helmF2 = assets.male.helms[cache.helm];
-    clothF = assets.male.clothing[cache.apparel];
-    weapon1M = assets.male.weapons[cache.weapon];
-    weapon2M = assets.male.weapons[cache.weapon];
-    weapon1F = assets.male.weapons[cache.weapon];
-    weapon2F = assets.male.weapons[cache.weapon];
-    bodyM = assets.male.bodies[identity.race].body;
-    bodyF = assets.male.bodies[identity.race].body;
-    headM = assets.male.bodies[identity.race].head;
-    headF = assets.male.bodies[identity.race].head;
-    handM = assets.male.bodies[identity.race].handMelee;
-    handF = assets.male.bodies[identity.race].handMelee;
     let body = assets.male.bodies[identity.race];
     const helmMapping = assets.male.helms[cache.helm];
     const wpnMapping = assets.male.weapons[cache.weapon];
@@ -110,27 +116,13 @@ function App() {
           {clothM2 && <image href={`${maleBaseUrl}/cloth/${clothM2}.png`}/>}
         </svg>
     );
-    imageF = (
-        <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 1200 1200">
-          <image href={bg}/>
-          <image href={weapon1F}/>
-          <image href={helmF1}/>
-          <image href={bodyF}/>
-          <image href={clothF}/>
-          <image href={handF}/>
-          <image href={weapon2F}/>
-          <image href={headF}/>
-          <image href={helmF2}/>
-        </svg>
-    );
   }
-
 
   return (
       <div>
         <h3 style={{textAlign: 'center'}}>Citizen Assembler</h3>
         <h5 style={{textAlign: 'center', marginTop: -10}}>Choose components to get estimated points and ranking of an assembled citizen</h5>
-        <div style={{display: 'flex', justifyContent: 'center'}}>
+        <div style={{display: 'flex', justifyContent: 'center', flexDirection: isMobile ? 'column' : 'row'}}>
           <span style={{marginRight: 5}}>Identity</span>
           <select style={{minWidth: 100, marginRight: 10}} onChange={e => setIdentity(identities[e.target.value] || null)}>
             <option value={''}>---</option>
@@ -157,20 +149,31 @@ function App() {
         </div>
         {isValidCitizen && (
             <>
-              <div style={{display: 'flex', justifyContent: 'center', marginTop: 40, flexDirection: 'column', alignItems: 'center'}}>
-                <span style={{display: 'block'}}>Estimated Points: {componentScore}</span>
-                <span style={{display: 'block', marginTop: 20}}>Estimated Rank: {rank}</span>
+              <div style={{display: 'flex', justifyContent: 'center', marginTop: 20, flexDirection: 'row', alignItems: 'center', marginBottom: 20,}}>
+
               </div>
-              <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center', width: '100%'}}>
-                <div style={{height: 450, width: 450}}>
+              <div style={{display: 'flex', flexDirection: isMobile ? 'column' : 'row', alignItems: 'center', justifyContent: 'center', width: '100%'}}>
+                <div style={{display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', marginRight: isMobile ? 0 : 50}}>
+                  <h4 style={{marginTop: 10, marginBottom: 10}}>Estimated Rank: <b>{rank}</b></h4>
+                  <h4 style={{marginTop: 10, marginBottom: 10}}>Estimated Points: <b>{componentScore}</b></h4>
+                  <h4 style={{marginTop: 10, marginBottom: 10}}>Attributes</h4>
+                  {Object.keys(attributes).map(attribute => (
+                    <div key={attribute} style={{display: 'flex', width: 300, justifyContent: 'space-between'}}>
+                      <span>{attribute}:</span>
+                      <span>{attributes[attribute]}</span>
+                    </div>
+                  ))}
+                </div>
+                <div style={{display: 'flex', height: 450, width: 450, flexDirection: 'column', alignItems: 'center'}}>
                   <h4 style={{textAlign: 'center', marginTop: 5, marginBottom: 5}}>Male</h4>
                   {imageM}
                 </div>
-                <div style={{height: 450, width: 450, marginLeft: 50}}>
+                <div style={{display: 'flex', height: 450, width: 450, marginLeft: isMobile ? 0 : 50, flexDirection: 'column', alignItems: 'center'}}>
                   <h4 style={{textAlign: 'center', marginTop: 5, marginBottom: 5}}>Female</h4>
                   {imageM}
                 </div>
               </div>
+
             </>
         )}
       </div>
